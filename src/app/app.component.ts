9df4e6f273e69862ddfc02fa6cfb63eb17d20fcf
lautiland <ljovanovics@fi.uba.ar>
2024-10-03 01:03:35 +0000

    feat[header]: sin logo, item disable ni refactor

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  categorias = [
+    'Inicio',
+    'Organización',
+    'Teoría',
+    'Campismo',
+    'Historia',
+    'Biblioteca'
+  ]
+}
