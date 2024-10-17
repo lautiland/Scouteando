5c3d70c0dcd74de66cae6f15c4e35050d044f500
lautiland <ljovanovics@fi.uba.ar>
2024-10-17 20:34:25 +0000

    fix[Header]: mostrar menú desktop / generar menú

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { NgForOf } from '@angular/common';
+
+interface Categorias {
+  [key: string]: string; 
+}
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  
+  categorias: Categorias = {
+    organizacion: 'Organización',
+    espiritu: 'Espíritu',
+    historia: 'Historia',
+    campismo: 'Campismo',
+    biblioteca: 'Biblioteca'
+  };
+
+  categoriasKeys = Object.keys(this.categorias); 
 }
